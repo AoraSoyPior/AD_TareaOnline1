c011db5ec1658506d5c96324a1e8a371187d059a
hugo <hgalpanez@gmail.com>
2025-11-29 20:21:57 +0100

    FabricaFuenteDatos cambiada

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/FabricaFuenteDatos.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/FabricaFuenteDatos.java
@@ -1,5 +1,6 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
 
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.ficheros.json.FuenteDatosFicherosJSON;
 import org.iesalandalus.programacion.tallermecanico.modelo.negocio.ficheros.xml.FuenteDatosFicherosXML;
 
 
@@ -10,6 +11,11 @@ public enum FabricaFuenteDatos {
         public IFuenteDatos crear() {
             return new FuenteDatosFicherosXML();
         }
+    },
+
+    FICHEROS_JSON {
+        @Override
+        public IFuenteDatos crear() { return new FuenteDatosFicherosJSON(); }
     };
 
     public abstract IFuenteDatos crear();
